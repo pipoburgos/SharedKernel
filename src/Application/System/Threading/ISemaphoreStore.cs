593c706186fa53c026010b5e01f38413775109a9
Roberto Fernández Salinas <roberto.salinas@e.ineco.com>
2021-08-09 13:57:18 +0200

    Manage semaphores by key (Thread Safe)

---
+++ src/Application/System/Threading/ISemaphoreStore.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading;
+
+namespace SharedKernel.Application.System.Threading
+{
+    /// <summary>
+    /// Create and manage SemaphoreSlim by dictionary keys
+    /// </summary>
+    public interface ISemaphoreStore
+    {
+        /// <summary>
+        /// Gets o create a SemaphoreSlim per key (Thread Safe)
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        SemaphoreSlim GetOrCreate(string key);
+    }
+}
---
+++ src/Infrastructure/System/Threading/CustomSemaphore.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using SharedKernel.Application.System.Threading;
+
+namespace SharedKernel.Infrastructure.System.Threading
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class CustomSemaphore : ISemaphore
+    {
+        private readonly ISemaphoreStore _semaphoreStore;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="semaphoreStore"></param>
+        public CustomSemaphore(ISemaphoreStore semaphoreStore)
+        {
+            _semaphoreStore = semaphoreStore;
+        }
+
+        /// <summary>
+        /// Create a SemaphoreSlim per key an block threads from given key
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="key"></param>
+        /// <param name="funcion"></param>
+        /// <returns></returns>
+        public async Task<T> RunOneAtATimeFromGivenKey<T>(string key, Func<T> funcion)
+        {
+            var semaforo = _semaphoreStore.GetOrCreate(key);
+            try
+            {
+                await semaforo.WaitAsync();
+                return funcion();
+            }
+            finally
+            {
+                semaforo.Release();
+            }
+        }
+
+        /// <summary>
+        /// Create a SemaphoreSlim per key an block threads from given key
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="key"></param>
+        /// <param name="funcion"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        public async Task<T> RunOneAtATimeFromGivenKey<T>(string key, Func<Task<T>> funcion, CancellationToken cancellationToken)
+        {
+            var semaforo = _semaphoreStore.GetOrCreate(key);
+            try
+            {
+                await semaforo.WaitAsync(cancellationToken);
+                return await funcion();
+            }
+            finally
+            {
+                semaforo.Release();
+            }
+        }
+    }
+}
