d3946728c1e6a47597122297d10f65ae5420c677
Roberto Fernández Salinas <roberto.salinas@ineco.com>
2021-09-10 15:45:22 +0200

    BaseController  - Added Streaming method  - New OkTyped method

--- src/Api/Controllers/BaseController.cs
+++ src/Api/Controllers/BaseController.cs
@@ -1,5 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Net.Http.Headers;
 using SharedKernel.Application.Cqrs.Commands;
 using SharedKernel.Application.Cqrs.Queries;
 
@@ -20,5 +24,42 @@ namespace SharedKernel.Api.Controllers
         /// Gets de query bus
         /// </summary>
         protected IQueryBus QueryBus => HttpContext.RequestServices.GetRequiredService<IQueryBus>();
+
+
+        /// <summary>
+        /// Creates a <see cref="ActionResult&lt;Value&gt;"/> object that produces an <see cref="StatusCodes.Status200OK"/> response.
+        /// </summary>
+        /// <returns>The created <see cref="ActionResult&lt;Value&gt;"/> for the response.</returns>
+        protected ActionResult<T> OkTyped<T>(T result) => Ok(result);
+
+        /// <summary>
+        /// Creates a <see cref="IActionResult"/> object that produces an empty <see cref="StatusCodes.Status200OK"/> response.
+        /// </summary>
+        /// <returns>The created <see cref="IActionResult"/> for the response.</returns>
+        protected IActionResult OkTyped() => Ok();
+
+        /// <summary>
+        /// Read a file an return in streaming
+        /// </summary>
+        /// <param name="filePath"></param>
+        protected async Task Streaming(string filePath)
+        {
+            Response.StatusCode = 200;
+            Response.Headers.Add(HeaderNames.ContentDisposition, $"attachment; filename=\"{Path.GetFileName(filePath)}\"");
+            Response.Headers.Add(HeaderNames.ContentType, "application/octet-stream");
+            await using var inputStream = new FileStream(filePath, FileMode.Open, FileAccess.Read);
+            var outputStream = Response.Body;
+            const int bufferSize = 1 << 10;
+            var buffer = new byte[bufferSize];
+            while (true)
+            {
+                var bytesRead = await inputStream.ReadAsync(buffer, 0, bufferSize);
+                if (bytesRead == 0)
+                    break;
+
+                await outputStream.WriteAsync(buffer, 0, bytesRead);
+            }
+            await outputStream.FlushAsync();
+        }
     }
 }
