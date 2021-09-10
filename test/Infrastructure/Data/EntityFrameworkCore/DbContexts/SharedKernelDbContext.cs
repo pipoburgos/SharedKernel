631ab5e4710e1b2265bfd56590a153729eca7678
Roberto Fernández Salinas <roberto.salinas@ineco.com>
2021-09-10 15:47:47 +0200

    New version 5.0.22

--- test/Infrastructure/Data/EntityFrameworkCore/DbContexts/SharedKernelDbContext.cs
+++ test/Infrastructure/Data/EntityFrameworkCore/DbContexts/SharedKernelDbContext.cs
@@ -1,17 +1,11 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using SharedKernel.Domain.Events;
 using SharedKernel.Infrastructure.Data.EntityFrameworkCore.DbContexts;
 
 namespace SharedKernel.Integration.Tests.Data.EntityFrameworkCore.DbContexts
 {
     public class SharedKernelDbContext : DbContextBase
     {
-        public SharedKernelDbContext(DbContextOptions<SharedKernelDbContext> options,
-            IAuditableService auditable = null, IEventBus eventBus = null) : base(options, "skr",
-            typeof(SharedKernelDbContext).Assembly, auditable, eventBus)
-        {
-
-        }
-
+        public SharedKernelDbContext(DbContextOptions<SharedKernelDbContext> options, IAuditableService auditable = null)
+            : base(options, "skr", typeof(SharedKernelDbContext).Assembly, auditable) { }
     }
 }
