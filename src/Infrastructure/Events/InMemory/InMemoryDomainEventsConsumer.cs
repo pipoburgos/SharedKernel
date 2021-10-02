e8e8d6e0f8fba549cf34cd31b4b234443e797d92
Roberto Fernández Salinas <roberto.salinas@ineco.com>
2021-10-02 15:38:01 +0200

    Version 5.0.28

--- src/Infrastructure/Events/InMemory/InMemoryDomainEventsConsumer.cs
+++ src/Infrastructure/Events/InMemory/InMemoryDomainEventsConsumer.cs
@@ -6,23 +6,31 @@ namespace SharedKernel.Infrastructure.Events.InMemory
     {
         private readonly DomainEventsToExecute _domainEventsToExecute;
         private readonly DomainEventMediator _domainEventMediator;
+        private readonly DomainEventJsonSerializer _serializer;
+        private readonly DomainEventJsonDeserializer _deserializer;
 
         public InMemoryDomainEventsConsumer(
             DomainEventsToExecute domainEventsToExecute,
-            DomainEventMediator domainEventMediator)
+            DomainEventMediator domainEventMediator,
+            DomainEventJsonSerializer serializer,
+            DomainEventJsonDeserializer deserializer)
         {
             _domainEventsToExecute = domainEventsToExecute;
             _domainEventMediator = domainEventMediator;
+            _serializer = serializer;
+            _deserializer = deserializer;
         }
 
         public void Consume(DomainEvent @event)
         {
             var subscribers = DomainEventSubscriberInformationService.GetAllEventsSubscribers(@event);
 
+            var eventSerialized = _serializer.Serialize(@event);
+            var eventDeserialized = _deserializer.Deserialize(eventSerialized);
             foreach (var subscriber in subscribers)
             {
                 _domainEventsToExecute.Add(cancellationToken =>
-                    _domainEventMediator.ExecuteOn(@event, subscriber, cancellationToken));
+                    _domainEventMediator.ExecuteOn(eventDeserialized, subscriber, cancellationToken));
             }
         }
     }
