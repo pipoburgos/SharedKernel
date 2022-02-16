83cdfcf8b9b70f0f41c162add506554b0db85b8d
Roberto Fernández Salinas <roberto.salinas@ineco.com>
2022-02-16 09:26:05 +0100

    Add GetBankAccountBalance query request

---
+++ samples/BankAccounts/src/Application/BankAccounts.Application/BankAccounts/Queries/GetBankAccountBalance.cs
@@ -0,0 +1,15 @@
+﻿using SharedKernel.Application.Cqrs.Queries;
+using System;
+
+namespace BankAccounts.Application.BankAccounts.Queries
+{
+    public class GetBankAccountBalance : IQueryRequest<decimal>
+    {
+        public GetBankAccountBalance(Guid bankAccountId)
+        {
+            BankAccountId = bankAccountId;
+        }
+
+        public Guid BankAccountId { get; }
+    }
+}
---
+++ samples/BankAccounts/src/Infrastructure/BankAccounts.Infrastructure/BankAccounts/Queries/GetBankAccountBalanceHandler.cs
@@ -0,0 +1,26 @@
+﻿using BankAccounts.Application.BankAccounts.Queries;
+using BankAccounts.Domain.BankAccounts.Repository;
+using SharedKernel.Infrastructure.Cqrs.Queries;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BankAccounts.Infrastructure.BankAccounts.Queries
+{
+    internal class GetBankAccountBalanceHandler : IQueryRequestHandler<GetBankAccountBalance, decimal>
+    {
+        private readonly IBankAccountRepository _bankAccountRepository;
+
+        public GetBankAccountBalanceHandler(
+            IBankAccountRepository bankAccountRepository)
+        {
+            _bankAccountRepository = bankAccountRepository;
+        }
+
+        public async Task<decimal> Handle(GetBankAccountBalance query, CancellationToken cancellationToken)
+        {
+            var bankAccount = await _bankAccountRepository.GetByIdAsync(query.BankAccountId, cancellationToken);
+
+            return bankAccount.Balance;
+        }
+    }
+}
