a7c86ecd01b545ebf5fa0fbae2b8a99556bce797
Roberto Fernández Salinas <roberto.salinas@ineco.com>
2022-02-21 09:22:48 +0100

    Created CreateBankAccountValidator from FluentValidator library.

---
+++ samples/BankAccounts/src/Infrastructure/BankAccounts.Infrastructure/BankAccounts/Commands/Validators/CreateBankAccountValidator.cs
@@ -0,0 +1,24 @@
+﻿using BankAccounts.Application.BankAccounts.Commands;
+using BankAccounts.Domain.BankAccounts.Repository;
+using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace BankAccounts.Infrastructure.BankAccounts.Commands.Validators
+{
+    // FluentValidation
+    internal class CreateBankAccountValidator : AbstractValidator<CreateBankAccount>
+    {
+        public CreateBankAccountValidator(IServiceProvider serviceProvider)
+        {
+            RuleFor(e => e.Id)
+                .NotEmpty()
+                .MustAsync(async (prop, c) => !await
+                    serviceProvider.GetRequiredService<IBankAccountRepository>().AnyAsync(prop, c));
+
+            RuleFor(e => e.Name)
+                .NotEmpty()
+                .MaximumLength(100);
+        }
+    }
+}
