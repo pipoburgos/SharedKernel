aba66d0448c4e1c1bdd067f52a607ff738a9e814
Roberto Fernández Salinas <roberto.salinas@ineco.com>
2022-07-24 02:40:14 +0200

    Pull domain events.

--- src/Application/Extensions/AggregateRootExtensions.cs
+++ src/Application/Extensions/AggregateRootExtensions.cs
@@ -1,22 +1,17 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using SharedKernel.Domain.Aggregates;
+﻿using SharedKernel.Domain.Aggregates;
 using SharedKernel.Domain.Events;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace SharedKernel.Application.Extensions
 {
-    /// <summary>
-    /// AggregateRoot extensions
-    /// </summary>
+    /// <summary> AggregateRoot extensions. </summary>
     public static class AggregateRootExtensions
     {
-        /// <summary>
-        /// Pull all domain events
-        /// </summary>
-        /// <typeparam name="T"></typeparam>
+        /// <summary> Pull all domain events. </summary>
         /// <param name="aggregateRoots"></param>
         /// <returns></returns>
-        public static IEnumerable<DomainEvent> PullDomainEvents<T>(this IEnumerable<AggregateRoot<T>> aggregateRoots)
+        public static IEnumerable<DomainEvent> PullDomainEvents(this IEnumerable<IAggregateRoot> aggregateRoots)
         {
             return aggregateRoots.SelectMany(a => a.PullDomainEvents());
         }
