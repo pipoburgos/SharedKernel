54bc2d9be05d5e2c090c329febc98fb0ecf7592d
Roberto Fernández Salinas <roberto.salinas@ineco.com>
2023-03-27 13:20:48 +0200

    Added AddElasticsearch method.

---
+++ src/Infrastructure/Data/Elasticsearch/ElasticsearchServiceExtensions.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Nest;
+using SharedKernel.Infrastructure.Data.Elasticsearch.Client;
+using System;
+
+namespace SharedKernel.Infrastructure.Data.Elasticsearch
+{
+    /// <summary>  </summary>
+    public static class ElasticsearchServiceExtensions
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="services"></param>
+        /// <param name="uri"></param>
+        /// <param name="defaultIndex"></param>
+        /// <param name="serviceLifetime"></param>
+        /// <returns></returns>
+        public static IServiceCollection AddElasticsearch(this IServiceCollection services, Uri uri,
+            string defaultIndex, ServiceLifetime serviceLifetime = ServiceLifetime.Scoped)
+        {
+            services.Add(new ServiceDescriptor(typeof(ElasticClient),
+                _ => new ElasticClient(new ConnectionSettings(uri).DefaultIndex(defaultIndex)), serviceLifetime));
+
+            services.Add(new ServiceDescriptor(typeof(ElasticsearchClient),
+                s => new ElasticsearchClient(s.GetRequiredService<ElasticClient>(),
+                    defaultIndex), serviceLifetime));
+
+            services
+                .AddHealthChecks()
+                .AddElasticsearch(uri.ToString());
+
+            return services;
+        }
+    }
+}
