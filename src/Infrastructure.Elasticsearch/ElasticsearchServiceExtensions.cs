f6c6bdcdf171536ea7af5de0caea57f1ea807275
Roberto Fernández Salinas <roberto.salinas@ineco.com>
2023-08-19 22:47:55 +0200

    Moved Elasticsearch to package Pipoburgos.SharedKernel.Infrastructure.Elasticsearch

---
+++ src/Infrastructure.Elasticsearch/ElasticsearchServiceExtensions.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Nest;
+using SharedKernel.Infrastructure.Elasticsearch.Client;
+
+namespace SharedKernel.Infrastructure.Elasticsearch;
+
+/// <summary>  </summary>
+public static class ElasticsearchServiceExtensions
+{
+    /// <summary>  </summary>
+    public static IServiceCollection AddElasticsearch(this IServiceCollection services, Uri uri,
+        string defaultIndex, ServiceLifetime serviceLifetime = ServiceLifetime.Scoped)
+    {
+        services.Add(new ServiceDescriptor(typeof(ElasticClient),
+            _ => new ElasticClient(new ConnectionSettings(uri).DefaultIndex(defaultIndex)), serviceLifetime));
+
+        services.Add(new ServiceDescriptor(typeof(ElasticsearchClient),
+            s => new ElasticsearchClient(s.GetRequiredService<ElasticClient>(),
+                defaultIndex), serviceLifetime));
+
+        services
+            .AddHealthChecks()
+            .AddElasticsearch(uri.ToString(), "Elasticsearch", tags: new[] { "DB", "Elasticsearch" });
+
+        return services;
+    }
+}
---
+++ src/Infrastructure.Elasticsearch/Repositories/ElasticsearchRepositoryAsync.cs
@@ -0,0 +1,32 @@
+﻿using SharedKernel.Domain.Aggregates;
+using SharedKernel.Domain.Repositories;
+using SharedKernel.Infrastructure.Elasticsearch.Client;
+
+namespace SharedKernel.Infrastructure.Elasticsearch.Repositories;
+
+/// <summary>  </summary>
+public abstract class ElasticsearchRepositoryAsync<TAggregateRoot> : ElasticsearchRepository<TAggregateRoot>,
+    IPersistRepositoryAsync where TAggregateRoot : class, IAggregateRoot
+{
+    /// <summary>  </summary>
+    /// <param name="client"></param>
+    protected ElasticsearchRepositoryAsync(ElasticsearchClient client) : base(client)
+    {
+    }
+
+    /// <summary>  </summary>
+    /// <param name="cancellationToken">Propagates notification that operations should be canceled.</param>
+    /// <returns></returns>
+    public Task<int> RollbackAsync(CancellationToken cancellationToken)
+    {
+        return Task.FromResult(0);
+    }
+
+    /// <summary>  </summary>
+    /// <param name="cancellationToken">Propagates notification that operations should be canceled.</param>
+    /// <returns></returns>
+    public Task<int> SaveChangesAsync(CancellationToken cancellationToken)
+    {
+        return Task.FromResult(0);
+    }
+}
