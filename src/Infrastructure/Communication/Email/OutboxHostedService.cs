38cf1c4c20b73a43eaf855f59b18444016500094
Roberto Fernández Salinas <roberto.salinas@ineco.com>
2023-09-21 10:48:08 +0200

    Fix smtp

---
+++ src/Infrastructure/Communication/Email/OutboxHostedService.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.Extensions.Hosting;
+
+namespace SharedKernel.Infrastructure.Communication.Email;
+internal class OutboxHostedService : BackgroundService
+{
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        throw new NotImplementedException();
+    }
+}
