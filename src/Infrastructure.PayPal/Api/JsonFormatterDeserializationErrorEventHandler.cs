af7c7976857e618fcc7b2ff61adf2bd52ec898ee
Roberto Fernández Salinas <robertofernandez1983@gmail.com>
2024-11-05 21:41:57 +0100

    Remove PayPal temporaly

--- src/Infrastructure.PayPal/Api/JsonFormatterDeserializationErrorEventHandler.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace SharedKernel.Infrastructure.PayPal.Api;
-
-/// <summary>
-/// Event handler delegate for when an error is encountered while deserializing a JSON string.
-/// </summary>
-/// <param name="e"></param>
-public delegate void JsonFormatterDeserializationErrorEventHandler(
-    JsonFormatterDeserializationErrorEventArgs e);
