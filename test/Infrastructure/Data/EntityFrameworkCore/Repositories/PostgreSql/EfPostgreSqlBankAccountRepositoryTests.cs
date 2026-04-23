00ecbc3c5dac81ceea26321a8dbe7d84a4df7845
Roberto Fernández Salinas <robertofernandez1983@gmail.com>
2026-04-23 17:26:56 +0200

    Version 10.0.7.1

--- test/Infrastructure/Data/EntityFrameworkCore/Repositories/PostgreSql/EfPostgreSqlBankAccountRepositoryTests.cs
+++ test/Infrastructure/Data/EntityFrameworkCore/Repositories/PostgreSql/EfPostgreSqlBankAccountRepositoryTests.cs
@@ -1,33 +1,33 @@
-﻿using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using SharedKernel.Infrastructure.EntityFrameworkCore.PostgreSQL.Data;
-using SharedKernel.Integration.Tests.Data.CommonRepositoryTesting;
-using SharedKernel.Integration.Tests.Data.EntityFrameworkCore.Repositories.PostgreSql.DbContexts;
-using SharedKernel.Integration.Tests.Data.EntityFrameworkCore.Repositories.PostgreSql.Repositories;
+﻿//using Microsoft.EntityFrameworkCore;
+//using Microsoft.Extensions.Configuration;
+//using Microsoft.Extensions.DependencyInjection;
+//using SharedKernel.Infrastructure.EntityFrameworkCore.PostgreSQL.Data;
+//using SharedKernel.Integration.Tests.Data.CommonRepositoryTesting;
+//using SharedKernel.Integration.Tests.Data.EntityFrameworkCore.Repositories.PostgreSql.DbContexts;
+//using SharedKernel.Integration.Tests.Data.EntityFrameworkCore.Repositories.PostgreSql.Repositories;
 
-namespace SharedKernel.Integration.Tests.Data.EntityFrameworkCore.Repositories.PostgreSql;
+//namespace SharedKernel.Integration.Tests.Data.EntityFrameworkCore.Repositories.PostgreSql;
 
 
-public class EfPostgreSqlBankAccountRepositoryTests : BankAccountRepositoryCommonTestTests<EfPostgreSqlBankAccountRepository>
-{
-    protected override string GetJsonFile()
-    {
-        return "Data/EntityFrameworkCore/Repositories/PostgreSql/appsettings.postgreSql.json";
-    }
+//public class EfPostgreSqlBankAccountRepositoryTests : BankAccountRepositoryCommonTestTests<EfPostgreSqlBankAccountRepository>
+//{
+//    protected override string GetJsonFile()
+//    {
+//        return "Data/EntityFrameworkCore/Repositories/PostgreSql/appsettings.postgreSql.json";
+//    }
 
-    public override void BeforeStart()
-    {
-        var dbContext = GetRequiredService<PostgreSqlSharedKernelDbContext>();
-        //dbContext.Database.EnsureDeleted();
-        dbContext.Database.Migrate();
-    }
+//    public override void BeforeStart()
+//    {
+//        var dbContext = GetRequiredService<PostgreSqlSharedKernelDbContext>();
+//        //dbContext.Database.EnsureDeleted();
+//        dbContext.Database.Migrate();
+//    }
 
-    protected override IServiceCollection ConfigureServices(IServiceCollection services)
-    {
-        var connection = Configuration.GetConnectionString("RepositoryConnectionString")!;
-        return services
-            .AddSharedKernelEntityFrameworkCorePostgreSqlDbContext<PostgreSqlSharedKernelDbContext>(connection)
-            .AddTransient<EfPostgreSqlBankAccountRepository>();
-    }
-}
+//    protected override IServiceCollection ConfigureServices(IServiceCollection services)
+//    {
+//        var connection = Configuration.GetConnectionString("RepositoryConnectionString")!;
+//        return services
+//            .AddSharedKernelEntityFrameworkCorePostgreSqlDbContext<PostgreSqlSharedKernelDbContext>(connection)
+//            .AddTransient<EfPostgreSqlBankAccountRepository>();
+//    }
+//}
--- test/Infrastructure/Data/EntityFrameworkCore/Repositories/PostgreSql/EfPostgreSqlUserUnitOfWorkTests.cs
+++ test/Infrastructure/Data/EntityFrameworkCore/Repositories/PostgreSql/EfPostgreSqlUserUnitOfWorkTests.cs
@@ -1,34 +1,34 @@
-﻿using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using SharedKernel.Infrastructure.EntityFrameworkCore.PostgreSQL.Data;
-using SharedKernel.Integration.Tests.Data.CommonRepositoryTesting;
-using SharedKernel.Integration.Tests.Data.EntityFrameworkCore.Repositories.PostgreSql.DbContexts;
-using SharedKernel.Integration.Tests.Data.EntityFrameworkCore.Repositories.PostgreSql.Repositories;
+﻿//using Microsoft.EntityFrameworkCore;
+//using Microsoft.Extensions.Configuration;
+//using Microsoft.Extensions.DependencyInjection;
+//using SharedKernel.Infrastructure.EntityFrameworkCore.PostgreSQL.Data;
+//using SharedKernel.Integration.Tests.Data.CommonRepositoryTesting;
+//using SharedKernel.Integration.Tests.Data.EntityFrameworkCore.Repositories.PostgreSql.DbContexts;
+//using SharedKernel.Integration.Tests.Data.EntityFrameworkCore.Repositories.PostgreSql.Repositories;
 
-namespace SharedKernel.Integration.Tests.Data.EntityFrameworkCore.Repositories.PostgreSql;
+//namespace SharedKernel.Integration.Tests.Data.EntityFrameworkCore.Repositories.PostgreSql;
 
 
-public class EfPostgreSqlUserUnitOfWorkTests : UserUnitOfWorkTests<EfPostgreSqlUserRepository, IPostgreSqlSharedKernelUnitOfWork>
-{
-    protected override string GetJsonFile()
-    {
-        return "Data/EntityFrameworkCore/Repositories/PostgreSql/appsettings.postgreSql.json";
-    }
+//public class EfPostgreSqlUserUnitOfWorkTests : UserUnitOfWorkTests<EfPostgreSqlUserRepository, IPostgreSqlSharedKernelUnitOfWork>
+//{
+//    protected override string GetJsonFile()
+//    {
+//        return "Data/EntityFrameworkCore/Repositories/PostgreSql/appsettings.postgreSql.json";
+//    }
 
-    public override void BeforeStart()
-    {
-        var dbContext = GetRequiredService<PostgreSqlSharedKernelDbContext>();
-        //dbContext.Database.EnsureDeleted();
-        dbContext.Database.Migrate();
-    }
+//    public override void BeforeStart()
+//    {
+//        var dbContext = GetRequiredService<PostgreSqlSharedKernelDbContext>();
+//        //dbContext.Database.EnsureDeleted();
+//        dbContext.Database.Migrate();
+//    }
 
-    protected override IServiceCollection ConfigureServices(IServiceCollection services)
-    {
-        var connection = Configuration.GetConnectionString("RepositoryConnectionString")!;
-        return services
-            .AddSharedKernelEntityFrameworkCorePostgreSqlUnitOfWork<IPostgreSqlSharedKernelUnitOfWork,
-                PostgreSqlSharedKernelDbContext>(connection)
-            .AddTransient<EfPostgreSqlUserRepository>();
-    }
-}
+//    protected override IServiceCollection ConfigureServices(IServiceCollection services)
+//    {
+//        var connection = Configuration.GetConnectionString("RepositoryConnectionString")!;
+//        return services
+//            .AddSharedKernelEntityFrameworkCorePostgreSqlUnitOfWork<IPostgreSqlSharedKernelUnitOfWork,
+//                PostgreSqlSharedKernelDbContext>(connection)
+//            .AddTransient<EfPostgreSqlUserRepository>();
+//    }
+//}
