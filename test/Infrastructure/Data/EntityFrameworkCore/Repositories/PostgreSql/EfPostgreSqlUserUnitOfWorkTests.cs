be4d8e8655290c55067694333f2afd3cc113168c
Roberto Fernández Salinas <robertofernandez1983@gmail.com>
2026-09-15 19:54:01 +0200

    Version 10.0.12.0

--- test/Infrastructure/Data/EntityFrameworkCore/Repositories/PostgreSql/EfPostgreSqlUserUnitOfWorkTests.cs
+++ test/Infrastructure/Data/EntityFrameworkCore/Repositories/PostgreSql/EfPostgreSqlUserUnitOfWorkTests.cs
@@ -1,34 +1,26 @@
-﻿//using Microsoft.EntityFrameworkCore;
-//using Microsoft.Extensions.Configuration;
-//using Microsoft.Extensions.DependencyInjection;
-//using SharedKernel.Infrastructure.EntityFrameworkCore.PostgreSQL.Data;
-//using SharedKernel.Integration.Tests.Data.CommonRepositoryTesting;
-//using SharedKernel.Integration.Tests.Data.EntityFrameworkCore.Repositories.PostgreSql.DbContexts;
-//using SharedKernel.Integration.Tests.Data.EntityFrameworkCore.Repositories.PostgreSql.Repositories;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SharedKernel.Infrastructure.EntityFrameworkCore.PostgreSQL.Data;
+using SharedKernel.Integration.Tests.Data.CommonRepositoryTesting;
+using SharedKernel.Integration.Tests.Data.EntityFrameworkCore.Repositories.PostgreSql.DbContexts;
+using SharedKernel.Integration.Tests.Data.EntityFrameworkCore.Repositories.PostgreSql.Repositories;
 
-//namespace SharedKernel.Integration.Tests.Data.EntityFrameworkCore.Repositories.PostgreSql;
+namespace SharedKernel.Integration.Tests.Data.EntityFrameworkCore.Repositories.PostgreSql;
 
 
-//public class EfPostgreSqlUserUnitOfWorkTests : UserUnitOfWorkTests<EfPostgreSqlUserRepository, IPostgreSqlSharedKernelUnitOfWork>
-//{
-//    protected override string GetJsonFile()
-//    {
-//        return "Data/EntityFrameworkCore/Repositories/PostgreSql/appsettings.postgreSql.json";
-//    }
+public class EfPostgreSqlUserUnitOfWorkTests : UserUnitOfWorkTests<EfPostgreSqlUserRepository, IPostgreSqlSharedKernelUnitOfWork>
+{
+    protected override string GetJsonFile()
+    {
+        return "Data/EntityFrameworkCore/Repositories/PostgreSql/appsettings.postgreSql.json";
+    }
 
-//    public override void BeforeStart()
-//    {
-//        var dbContext = GetRequiredService<PostgreSqlSharedKernelDbContext>();
-//        //dbContext.Database.EnsureDeleted();
-//        dbContext.Database.Migrate();
-//    }
-
-//    protected override IServiceCollection ConfigureServices(IServiceCollection services)
-//    {
-//        var connection = Configuration.GetConnectionString("RepositoryConnectionString")!;
-//        return services
-//            .AddSharedKernelEntityFrameworkCorePostgreSqlUnitOfWork<IPostgreSqlSharedKernelUnitOfWork,
-//                PostgreSqlSharedKernelDbContext>(connection)
-//            .AddTransient<EfPostgreSqlUserRepository>();
-//    }
-//}
+    protected override IServiceCollection ConfigureServices(IServiceCollection services)
+    {
+        var connection = Configuration.GetConnectionString("RepositoryConnectionString")!;
+        return services
+            .AddSharedKernelEntityFrameworkCorePostgreSqlUnitOfWork<IPostgreSqlSharedKernelUnitOfWork,
+                PostgreSqlSharedKernelDbContext>(connection)
+            .AddTransient<EfPostgreSqlUserRepository>();
+    }
+}
--- test/Infrastructure/Data/EntityFrameworkCore/Repositories/SqlServer/EfCoreSqlServerUserUnitOfWorkTests.cs
+++ test/Infrastructure/Data/EntityFrameworkCore/Repositories/SqlServer/EfCoreSqlServerUserUnitOfWorkTests.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
+﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using SharedKernel.Infrastructure.EntityFrameworkCore.SqlServer.Data;
 using SharedKernel.Integration.Tests.Data.CommonRepositoryTesting;
@@ -13,13 +12,6 @@ public class EfCoreSqlServerUserUnitOfWorkTests : UserUnitOfWorkTests<EfCoreUser
     protected override string GetJsonFile()
     {
         return "Data/EntityFrameworkCore/Repositories/SqlServer/appsettings.sqlServer.json";
-    }
-
-    public override void BeforeStart()
-    {
-        var dbContext = GetRequiredServiceOnNewScope<SharedKernelEntityFrameworkDbContext>();
-        //dbContext.Database.EnsureDeleted();
-        dbContext.Database.Migrate();
     }
 
     protected override IServiceCollection ConfigureServices(IServiceCollection services)
